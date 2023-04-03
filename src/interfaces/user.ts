0437b5bfd3b7f0f5971851c2a9d58d08c952e408
noahbuilds <edetnoah@gmail.com>
2023-04-03 15:16:03 +0100

    feat: worked on user model

---
+++ src/interfaces/user.ts
@@ -0,0 +1,16 @@
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  gender: string;
+  phoneNumber: string;
+  stateOfOrigin: string;
+  age: number;
+  interests: Array<string>;
+  level: number;
+  location: string;
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { IUser } from "../interfaces/user";
+
+const userSchema = new mongoose.Schema<IUser>({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true, unique: true, uppercase: false},
+    password: {type: String, required: true},
+    gender: {type: String, required: true},
+    phoneNumber: {type: String, required: true},
+    stateOfOrigin: {type: String},
+    age: {type: Number, required: true},
+    interests: {type: [String]},
+    level: {type: Number, required: true},
+    location: {type: String, required: true}
+}, {timestamps: true})
+
+const User = mongoose.model("User",userSchema);
+
+export {User}

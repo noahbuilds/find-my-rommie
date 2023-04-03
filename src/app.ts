4d0fcc1851651ceea353061e84d155e6341099b6
noahbuilds <edetnoah@gmail.com>
2023-04-03 14:54:34 +0100

    chore: db setup

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response }  from "express";
+import mongoose, { ConnectOptions } from "mongoose";
 import configuration from "./configs/configs";
 
 const app: Application = express()
@@ -11,6 +12,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+(async function () {
+    try {
+      await mongoose.connect(configuration.MONGO_URI!, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      } as ConnectOptions);
+      return console.log(`Successfully connected to ${configuration.MONGO_URI}`);
+    } catch (error:any) {
+      console.log("Error connecting to database: ", error);
+      return process.exit(1);
+    }
+  })();
 
 app.listen(configuration.ENV_PORT, () => {
     console.log(`App is running on PORT ${configuration.ENV_PORT}`);

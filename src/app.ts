6e2a8fbcb3718d80d0b8603d79f08738aacb0af2
noahbuilds <edetnoah@gmail.com>
2023-05-30 21:20:48 +0100

    fix:deployment error

--- src/app.ts
+++ src/app.ts
@@ -24,11 +24,11 @@ class App {
 
     private async initializeDatabaseConnection(): Promise<void> {
         try {
-            await mongoose.connect(this.mongoURI!, {
+            await mongoose.connect(process.env.MONGO_URI!, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
             } as ConnectOptions);
-            return console.log(`Successfully connected to ${this.mongoURI}`);
+            return console.log(`Successfully connected to ${process.env.MONGO_URI!}`);
         } catch (error) {
             console.log('Error connecting to database: ', error);
             return process.exit(1);

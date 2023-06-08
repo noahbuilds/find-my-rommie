0c7677cd109a68a85d8cfb7c4576a0ae86fe22b1
noahbuilds <edetnoah@gmail.com>
2023-06-09 00:44:52 +0100

    fix: env variables

--- backend/src/configs/firebase.config.ts
+++ backend/src/configs/firebase.config.ts
@@ -4,12 +4,12 @@ dotenv.config();
 
 export default {
     fireBaseConfig: {
-        apiKey: 'AIzaSyBUtiZBpdcsAPrWkqb_5M4igK5pebxNBHY',
-        authDomain: 'roommie-app.firebaseapp.com',
-        projectId: 'roommie-app',
-        storageBucket: 'roommie-app.appspot.com',
-        messagingSenderId: '791044382919',
-        appId: '1:791044382919:web:d536e65aff27ed009c5ee2',
-        measurementId: 'G-X11EGDRPR6',
+        apiKey: process.env.fireBaseApiKey,
+        authDomain: process.env.authDomain,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId,
+        appId: process.env.appId,
+        measurementId: process.env.measurementId,
     },
 };
